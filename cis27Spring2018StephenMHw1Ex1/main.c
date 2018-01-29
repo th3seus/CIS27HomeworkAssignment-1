86b782c775a279a73b5cce7ce8e972ac8ac6fb80
Stephen <macbook@Macbooks-MacBook-Pro-3.local>
2018-01-28 21:09:42 -0800

    added displayClassInforStephenM() function

--- cis27Spring2018StephenMHw1Ex1/main.c
+++ cis27Spring2018StephenMHw1Ex1/main.c
@@ -8,7 +8,22 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
+void displayClassInfoStephenM(void);
+int* displayEvenOddDigitInfoStephenM(int);
+int getLargestIntegerWithLargestTUEDCStephenM(int*);
 
+int main(int argc, const char * argv[]) {
+    displayClassInfoStephenM();
     return 0;
+}
+
+void displayClassInfoStephenM() {
+    printf("CIS 27 - Data Structures\n"
+           "Laney College\n"
+           "Stephen Miller\n\n"
+           "Assignment Information --\n"
+           "\tAssignment Number:\tHomework 1,\n"
+           "\t\t\t\t\t\tCoding Assignment -- Exercise #1\n"
+           "\tWritten By:\t\t\tStephen Miller\n"
+           "\tSubmitted Date:\t\t");
 }
