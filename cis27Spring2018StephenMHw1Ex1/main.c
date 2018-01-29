52e698a56caa03f229559c0db770a5e41577421e
Stephen <macbook@Macbooks-MacBook-Pro-3.local>
2018-01-28 21:33:46 -0800

    filled out skeleton of menu function minor edits to display class info function

--- cis27Spring2018StephenMHw1Ex1/main.c
+++ cis27Spring2018StephenMHw1Ex1/main.c
@@ -17,6 +17,8 @@ int getLargestIntegerWithLargestTUEDCStephenM(int*); // return largest user
 
 int main(int argc, const char * argv[]) {
     displayClassInfoStephenM();
+    
+    displayMenuHw1();
     return 0;
 }
 
@@ -25,8 +27,34 @@ void displayClassInfoStephenM() {
            "Laney College\n"
            "Stephen Miller\n\n"
            "Assignment Information --\n"
-           "\tAssignment Number:\tHomework 1,\n"
-           "\t\t\t\t\t\tCoding Assignment -- Exercise #1\n"
-           "\tWritten By:\t\t\tStephen Miller\n"
-           "\tSubmitted Date:\t\t");
+           "  Assignment Number:    Homework 1,\n"
+           "                        Coding Assignment -- Exercise #1\n"
+           "  Written By:           Stephen Miller\n"
+           "  Submitted Date:       2018/02/01\n\n");
+}
+
+void displayMenuHw1() {
+    // main menu program
+    int option = 0;
+    
+    do {
+        printf("****************************************\n"
+               "*             Menu - HW #1 -           *\n"
+               "* 1. Call getLargestIntegerWithTUEDC() *\n"
+               "* 2. Quit                              *\n"
+               "****************************************\n");
+        printf("Enter an integer for option + ENTER: ");
+        scanf("%d", &option);
+        
+        switch(option) {
+            case 1: // call getLargest...()
+                break;
+            case 2: // quit menu
+                printf("\nQuitting\n");
+                break;
+            default: // wrong option
+                printf("\nWRONG OPTION\n");
+                break;
+        }
+    } while (option != 2);
 }
